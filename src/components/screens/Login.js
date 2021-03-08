9c378e0bee663aa84b85ff1b8fba86651d6c2c4e
emoueitchaien <manojpradhan628@gmail.com>
2021-03-09 01:48:24 +0545

    css fixes

--- src/components/screens/Login.js
+++ src/components/screens/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import logo from "../photos/insss.png";
-import photo from "../photos/sigup.png";
+import photo from "../photos/sigupfinal.png";
 import photo2 from "../photos/photo2.png";
 import iosapp from "../photos/iosapp.png";
 import andapp from "../photos/andapp.png";
@@ -60,95 +60,97 @@ const Login = (props) => {
   // const [username, setUsername] = useState("");
 
   return (
-    <div className="mainbody">
-      <div className="signupphoto1">
-        <img className="photo1" src={photo} />
-        <img className="photo2" src={photo2} />
-      </div>
-      <div>
-        <div style={{ height: "382px" }} className="signin">
-          <img className="logo" src={logo} />
-          <input
-            className="browser-default inputfield"
-            type="text"
-            placeholder="Email"   //Phone number, username, or email
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            className="browser-default inputfield"
-            type="password"
-            placeholder="Password"
-            value={pass}
-            onChange={(e) => setPass(e.target.value)}
-          />
-          <button className="browser-default bttn" onClick={PostData}>
-            Log In
-          </button>
-          <br />
-          <div
-            style={{
-              height: "15px",
-              width: "268px",
-              marginTop: "12px",
-              marginLeft: "auto",
-              marginRight: "auto",
-            }}
-          >
-            <div className="s311c" style={{ float: "left" }}></div>
-            <div className="or">OR</div>
-            <div className="s311c" style={{ float: "right" }}></div>
-          </div>
-          <div className="logwidfb">
-            <span className="lwfimg">
-              <img height="16px" width="16px" src={fbicon} />
-            </span>
-            <span className="logtext">
-              <p className="lwftxt">Log in with Facebook</p>
-            </span>
-          </div>
-          <p
-            style={{
-              paddingTop: "14px",
-              paddingBottom: "5px",
-              fontFamily: "Helvetica",
-              fontSize: "12px",
-            }}
-          >
-            <a href="#" style={{ color: "#00376B" }}>
-              Forgot password?
-            </a>
-          </p>
+    <div className="mainbody"> 
+      <div style={{ width: "55%", marginTop : "3%" , marginLeft: "auto", marginRight:"auto"}}>
+        <div className="signupphoto1">
+          <img className="photo1" src={photo} />
+          {/* <img className="photo2" src={photo2} /> */}
         </div>
-        <div className="signin signin2">
-          <p>
-            Don't have an account?{" "}
-            <Link to="/signup" style={{ fontWeight: "bold" }}>
-              {" "}
-              Sign up
-            </Link>
-          </p>
-        </div>
-        <div className="getapp">
-          <p style={{ textAlign: "center" }}>Get the app.</p>
-          <div className="applink">
-            <a
-              target="_blank"
-              href="https://itunes.apple.com/app/instagram/id389801252?pt=428156&ct=igweb.loginPage.badge&mt=8&vt=lo"
+        <div className="allsignin">
+          <div style={{ height: "382px" }} className="signin">
+            <img className="logo" src={logo} />
+            <input
+              className="browser-default inputfield"
+              type="text"
+              placeholder="Email"   //Phone number, username, or email
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              className="browser-default inputfield"
+              type="password"
+              placeholder="Password"
+              value={pass}
+              onChange={(e) => setPass(e.target.value)}
+            />
+            <button className="browser-default bttn" onClick={PostData}>
+              Log In
+            </button>
+            <br />
+            <div
+              style={{
+                height: "15px",
+                width: "268px",
+                marginTop: "12px",
+                marginLeft: "auto",
+                marginRight: "auto",
+              }}
             >
-              <img height="40px" width="136px" src={iosapp} />
-            </a>
-            <a
-              target="_blank"
-              href="https://play.google.com/store/apps/details?id=com.instagram.android&referrer=utm_source%3Dinstagramweb%26utm_campaign%3DloginPage%26ig_mid%3D2812111B-61E2-42F2-8CA8-B9DF0F227CE0%26utm_content%3Dlo%26utm_medium%3Dbadge"
+              <div className="s311c" style={{ float: "left" }}></div>
+              <div className="or">OR</div>
+              <div className="s311c" style={{ float: "right" }}></div>
+            </div>
+            <div className="logwidfb">
+              <span className="lwfimg">
+                <img height="16px" width="16px" src={fbicon} />
+              </span>
+              <span className="logtext">
+                <p className="lwftxt">Log in with Facebook</p>
+              </span>
+            </div>
+            <p
+              style={{
+                paddingTop: "14px",
+                paddingBottom: "5px",
+                fontFamily: "Helvetica",
+                fontSize: "12px",
+              }}
             >
-              <img
-                style={{ marginLeft: "8px" }}
-                height="40px"
-                width="134.283px"
-                src={andapp}
-              />
-            </a>
+              <a href="#" style={{ color: "#00376B" }}>
+                Forgot password?
+              </a>
+            </p>
+          </div>
+          <div className="signin signin2">
+            <p>
+              Don't have an account?{" "}
+              <Link to="/signup" style={{ fontWeight: "bold" }}>
+                {" "}
+                Sign up
+              </Link>
+            </p>
+          </div>
+          <div className="getapp">
+            <p style={{ textAlign: "center" }}>Get the app.</p>
+            <div className="applink">
+              <a
+                target="_blank"
+                href="https://itunes.apple.com/app/instagram/id389801252?pt=428156&ct=igweb.loginPage.badge&mt=8&vt=lo"
+              >
+                <img height="40px" width="136px" src={iosapp} />
+              </a>
+              <a
+                target="_blank"
+                href="https://play.google.com/store/apps/details?id=com.instagram.android&referrer=utm_source%3Dinstagramweb%26utm_campaign%3DloginPage%26ig_mid%3D2812111B-61E2-42F2-8CA8-B9DF0F227CE0%26utm_content%3Dlo%26utm_medium%3Dbadge"
+              >
+                <img
+                  style={{ marginLeft: "8px" }}
+                  height="40px"
+                  width="134.283px"
+                  src={andapp}
+                />
+              </a>
+            </div>
           </div>
         </div>
       </div>
